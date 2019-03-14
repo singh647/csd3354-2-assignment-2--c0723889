b9ba35e454c2da8ef0c25591458babb3e5180d29
visualstudio <visualstudio@230-PC39>
2019-03-14 14:23:13 -0400

    lab 10

--- csd3354-2-assignment-2- c0723889/Program.cs
+++ csd3354-2-assignment-2- c0723889/Program.cs
@@ -9,9 +9,32 @@ namespace csd3354_2_assignment_2__c0723889
 //ramandeepsingh brar c0730408
 
 {
-    public delegate void MyDelegate();
-    public class DelegateExcercises : MyDelegate
+    public class Program
     {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+        }
+    }
 
+    public delegate void MyDelegate();
+
+    public class DelegateExercises
+    {
+        void Method1()
+        {
+            System.Console.WriteLine("Method1");
+
+
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+            System.Console.WriteLine(myDelegate.ToString());
+        }
     }
 }
