fca7d5c21d01003686a825c33090a75322f5642b
visualstudio <visualstudio@230-PC39>
2019-03-14 14:27:23 -0400

    lab 15

--- csd3354-2-assignment-2- c0723889/Program.cs
+++ csd3354-2-assignment-2- c0723889/Program.cs
@@ -9,32 +9,40 @@ namespace csd3354_2_assignment_2__c0723889
 //ramandeepsingh brar c0730408
 
 {
-public class Program
+
+    public class Program
     {
         public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
+            try
+            {
+                delegateExercises.Method3();
+                Console.ReadLine();
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Exception Occurred.");
+                Console.ReadLine();
+
+            }
         }
     }
-    public delegate int MyDelegate(out int i);
+
+    public delegate void MyDelegate();
 
     public class DelegateExercises
     {
-        int Method1(out int i)
+        void Method1()
         {
-            i = 100;
-            System.Console.WriteLine("Method1 " + i);
-            return 0;
+            throw new System.Exception();
         }
+
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = null;
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intValue;
-            myDelegate2(out intValue);
+            myDelegate();
         }
     }
+
 }
