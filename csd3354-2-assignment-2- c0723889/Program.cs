fa31479d73a7d0c9eb193bdac1433578b13aa03b
visualstudio <visualstudio@230-PC39>
2019-03-14 14:28:10 -0400

    lab 16

--- csd3354-2-assignment-2- c0723889/Program.cs
+++ csd3354-2-assignment-2- c0723889/Program.cs
@@ -15,33 +15,29 @@ namespace csd3354_2_assignment_2__c0723889
         public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
-            try
-            {
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
-            catch (System.Exception ex)
-            {
-                System.Console.WriteLine("Exception Occurred.");
-                Console.ReadLine();
+            delegateExercises.Method3();
+            Console.ReadLine();
 
-            }
         }
     }
 
-    public delegate void MyDelegate();
+    public delegate void MyDelegate(ref int intValue);
 
     public class DelegateExercises
     {
-        void Method1()
+        void Method1(ref int intValue)
         {
-            throw new System.Exception();
+            intValue = intValue + 5;
+            System.Console.WriteLine("Method1 " + intValue);
         }
 
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intParameter = 5;
+            myDelegate2(ref intParameter);
         }
     }
 
