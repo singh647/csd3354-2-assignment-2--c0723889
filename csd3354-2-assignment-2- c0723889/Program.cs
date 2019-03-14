fc29760b4a113687b2dabac6328da081845a9ca5
visualstudio <visualstudio@230-PC39>
2019-03-14 14:25:51 -0400

    lab 13

--- csd3354-2-assignment-2- c0723889/Program.cs
+++ csd3354-2-assignment-2- c0723889/Program.cs
@@ -8,39 +8,35 @@ namespace csd3354_2_assignment_2__c0723889
 //rupinderpalsingh c0723889
 //ramandeepsingh brar c0730408
 {
-    using System;
-
-    delegate void ExampleDelegate(string xyz);
-
-    class Program
+    public class Program
     {
-        public static void Method1(string xyz)
-        {
-            Console.WriteLine(xyz + " Method1");
-        }
-
-        public static void Method2(string xyz)
-        {
-            Console.WriteLine(xyz + " Method2");
-        }
-
         public static void Main()
         {
-            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
-
-            ex1Delegate = new ExampleDelegate(Method1);
-            ex2Delegate = new ExampleDelegate(Method2);
-            ex3Delegate = ex1Delegate + ex2Delegate;
-            myDelegate = ex1Delegate - ex2Delegate;
-            ex1Delegate("AAA");
-            ex2Delegate("BBB");
-            ex3Delegate("CCC");
-            myDelegate("DDD");
-            myDelegate = ex3Delegate - ex1Delegate;
-            myDelegate("EEE");
-            myDelegate = ex3Delegate - ex2Delegate;
-            myDelegate("FFF");
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
             Console.ReadLine();
+        }
+    }
+
+    public delegate int MyDelegate(out int i);
+
+    public class DelegateExercises
+    {
+        int Method1(out int i)
+        {
+            System.Console.WriteLine("Method1");
+            i = 10;
+            return 0;
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intValue;
+            myDelegate2(out intValue);
+
         }
     }
 }
